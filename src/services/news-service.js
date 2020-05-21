b86a30ee676f169a4110e1dc1adbf231630a0005
Дмитрий Мирошниченко <d.mir.posts@gmail.com>
2020-05-21 20:28:28 +0300

    Небольшие правки

--- src/services/news-service.js
+++ src/services/news-service.js
@@ -2,7 +2,7 @@ export default class NewsService {
   getData() {
     return new Promise((resolve, reject) => {
       setTimeout(async () => {
-        const res = await fetch('/assets/data/data.json');
+        const res = await fetch('./assets/data/data.json');
 
         if (!res.ok) {
           reject(new Error(`Could not fetch ${url}, received ${res.status}`));
